bc7d293fec743cc2887413ad7c769b64659110b3
Ahmed Magdy Ibrahim <magdz_008@yahoo.com>
2015-12-16 12:52:17 +0200

    initiate GUI

--- src/ShapeView.java
+++ src/ShapeView.java
@@ -14,7 +14,12 @@ public class ShapeView {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new GUI().setVisible(true);
+            }
+        });
     }
     
 }
